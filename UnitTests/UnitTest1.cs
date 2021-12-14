ada9532224e0b259582e5f565bd2fe4494a4cc11
Sif-on <e.simonovskiy@gmail.com>
2021-12-14 18:13:57 +0200

    Prepared by UnitTest.

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using GUI_for_DB;
+using System;
+
 
 namespace GUI_for_DB
 {
@@ -15,7 +18,18 @@ namespace GUI_for_DB
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+            
+        }
+
+
+        // Проверка конфига с паролями для подключения к БД.
+        [TestCase("./GUI_for_DB/MigratedPharmacyDatabase/", "App.Config", true)]
+        public void VectorAdd(string path, string name_file, bool expected)
+        {
+            bool result = System.IO.File.Exists($"{path}{name_file}");
+            if (expected != result)
+                Assert.Fail(message: $"Каталог по пути {path} не содержит файл {name_file}.");
         }
     }
 }
